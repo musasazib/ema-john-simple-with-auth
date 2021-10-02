1561c633ec31c9cd158dd400bef4729f70aec8d6
musasazib <musasazib@gmail.com>
2021-10-02 10:25:54 +0600

    order placed and clear cart and stored data

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -3,16 +3,26 @@ import useProducts from '../../hooks/useProducts';
 import useCart from '../../hooks/useCart';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
-import { removeFromDb } from '../../utilities/fakedb';
+import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
+import { useHistory } from 'react-router';
 
 const OrderReview = () => {
     const [products] = useProducts();
     const [cart, setCart] = useCart(products);
+    const history = useHistory();
+
     const handleRemove = key => {
         const newCart = cart.filter(product => product.key !== key);
         setCart(newCart);
         removeFromDb(key);
     }
+
+    const handlePlaceOrder = () => {
+        history.push("/placeorder");
+        setCart([]);
+        clearTheCart();
+    }
+
     return (
         <div className="shop-container">
             <div className="product-container">
@@ -26,7 +36,7 @@ const OrderReview = () => {
             </div>
             <div className="cart-container">
                 <Cart cart={cart}>
-                    <button className="btn-regular">Place Order</button>
+                    <button onClick={handlePlaceOrder} className="btn-regular">Place Order</button>
                 </Cart>
             </div>
         </div>
