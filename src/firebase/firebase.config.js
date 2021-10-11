f09478084c050010d18db759308a82118a046a0e
musasazib <musasazib@gmail.com>
2021-10-12 00:24:24 +0600

    Firebase initial setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkPrMMTqVX_M16NUYoVco5iqz1e2H4GWM",
+    authDomain: "ema-john-shopping-site.firebaseapp.com",
+    projectId: "ema-john-shopping-site",
+    storageBucket: "ema-john-shopping-site.appspot.com",
+    messagingSenderId: "970639086036",
+    appId: "1:970639086036:web:9aca946c00213a70198641"
+};
+  
+export default firebaseConfig;
