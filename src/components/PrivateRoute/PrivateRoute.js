cac91fb38269869ac49d0194f948b7b4a68d2601
musasazib <musasazib@gmail.com>
2021-10-12 22:19:11 +0600

    Added Private Route

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router';
+import { Redirect, Route } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 
 const PrivateRoute = ({ children, ...rest }) => {
-    const { user } = useAuth();
+    const { user, loading } = useAuth();
+    if (loading) return 'loading';
     return (
         <Route
-        {...rest}
-        render={({ location }) => user.email ? children
-            :<Redirect
+            {...rest}
+            render={({ location }) => user.email ? children : <Redirect
                 to={{
                     pathname: "/login",
                     state: { from: location }
                 }}
-            ></Redirect>
+            ></Redirect>}
         >
-            
+
         </Route>
     );
 };
